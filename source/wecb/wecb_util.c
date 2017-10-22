15df907dbfcb583cf950797b44796eb5c38109a0
Dave Smith <Dave.M.Smith@accenture.com>
2017-10-22 14:20:55 +0100

    RDKB-12226: Update to include curl license.
    Reason for change: Some code originates from Curl example threaded-ssl.c.
    Test Procedure: Build only.
    Risks: None
    Signed-off-by: Dave Smith <dave.m.smith@accenture.com>
    
    Change-Id: Id03d453824bcbed1bdb8f624588de7438c6fb31a

--- source/wecb/wecb_util.c
+++ source/wecb/wecb_util.c
@@ -31,7 +31,24 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 **********************************************************************/
-
+/***************************************************************************
+ * Some parts of this software are based on cURL threaded-ssl.c example code
+ * licensed under the following:
+ *
+ * Copyright (C) 1998 - 2017, Daniel Stenberg, <daniel@haxx.se>, et al.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution. The terms
+ * are also available at https://curl.haxx.se/docs/copyright.html.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the COPYING file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
 /**************************************************************************
 
     module: wecb_util.c
